08faadd0d7b78b1f2b011bc9543d84c07df6b845
shaberusman14 <113237652+shaberusman14@users.noreply.github.com>
2025-08-12 21:07:44 +0530

    Update routes.js

--- delivery-management-backend/src/routes/routes.js
+++ delivery-management-backend/src/routes/routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const Route = require('../models/route');
+const Route = require('../models/Route');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
